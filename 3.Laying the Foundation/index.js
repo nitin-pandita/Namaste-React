1ea925b81309a53daeff3e3c9953f351632ebbf7
nitin-pandita <nitin_207013@saitm.org>
2024-01-29 14:23:13 +0530

    Laying the Foundation

--- 3.Laying the Foundation/index.js
+++ 3.Laying the Foundation/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// using the react
+//* React.createElement => JS Object => HTML
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Welcome to Nitin React js Learning"
 );
 
+// jsx syntax
+//*  JSX Code => React.createElement => JS Object => HTML
+
+const jsxSyntax = <h1>Welcome to JSX </h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxSyntax);
